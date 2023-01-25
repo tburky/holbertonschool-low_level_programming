9c19616b92c361492a452f13702fbb89f3ceec55
Tanner <tanner.burkybile@gmail.com>
2023-01-25 11:30:10 -0800

    added file 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+/*
+ * file: 5.printf.c
+ * Auth: Tanner Burkybile <tanner.burkybile@gmail.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
