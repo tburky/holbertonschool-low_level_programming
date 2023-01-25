3533776b11e82147d1f6a40cd67b6dadc2d5ba33
Tanner <tanner.burkybile@gmail.com>
2023-01-25 10:25:57 -0800

    edited file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
+/* This will print a specific line requested by assignment */
 #include <stdio.h>
+/* main - Entry point
+ * This will print a specific text using puts command in c
+ */
 int main(void)
+/* This will take a char string and puts it */
 {
 	char Mystr[] = "\"Programming is like building a multilingual puzzle\n";
-
+	
 	puts(Mystr);
 
 		return (0);
