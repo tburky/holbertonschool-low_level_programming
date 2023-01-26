0a7536c29ef0e0ad5a8939de1b208874102adbd4
Tanner <tanner.burkybile@gmail.com>
2023-01-26 10:08:09 -0800

    edited file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+/*
+ * file: 0-positive_or_negative.c
+ * Auth: Tanner Burkybile <tanner.burkybile@gmail.com>
+ */
+
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'Z' ; ch++)
+	putchar (ch);
+
+	return (0);
+}
