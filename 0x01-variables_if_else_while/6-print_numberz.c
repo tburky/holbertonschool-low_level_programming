34d70d199561ad0bccdd41086ec8fdcee15fbfad
Tanner <tanner.burkybile@gmail.com>
2023-01-26 11:57:26 -0800

    added new file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+/*
+ * file: 6-print_number
+ * Auth: Tanner Burkybile <tanner.burkybile@gmail.com>
+ */
+
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+{
+	for (n = 0 ; n <= 9 ; n++)
+	putchar(48 + n);
+}
+	putchar (10);
+
+	return (0);
+}
