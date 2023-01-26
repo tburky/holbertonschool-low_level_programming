27ee77b5719ea4b1c5980adcf5c43ba5640acd29
Tanner <tanner.burkybile@gmail.com>
2023-01-26 12:07:55 -0800

    edited file and added file

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+/*
+ * file: 7-print_tebahpla.c
+ * Auth: Tanner Burkybile <tanner.burkybile@gmail.com>
+ */
+
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+{
+	for (n = 'z' ; n >= 'a' ; n--)
+	putchar(n);
+}
+	putchar (10);
+
+	return (0);
+}
