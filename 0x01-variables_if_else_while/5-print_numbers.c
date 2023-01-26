93d7ed41fee2220ad57847e47db15b025f68558a
Tanner <tanner.burkybile@gmail.com>
2023-01-26 11:43:57 -0800

    edited file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+/*
+ * file: 4-print_alphabets.c
+ * Auth: Tanner Burkybile <tanner.burkybile@gmail.com>
+ */
+
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n <= 9 ; n++)
+	printf("%d\n", n);
+
+	return (0);
+}
