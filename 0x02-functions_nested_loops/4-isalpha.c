a93d3f1bf2b71fcfdb27247798cf6f2b7b67b46c
Tanner <tanner.burkybile@gmail.com>
2023-01-27 13:03:12 -0800

    added file 4 and made edits

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _islower - Entry point
+* @c: lowercase
+* Return: 1 for true 0 for false
+*/
+int _islower(int c)
+{
+	if ((c > 96 && c <= 122) || (c > 64 && c <= 90 ))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
