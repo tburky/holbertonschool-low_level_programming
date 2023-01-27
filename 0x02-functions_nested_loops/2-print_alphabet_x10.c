88f534025f311d3d9a75670b68e5a8faea6e2744
Tanner <tanner.burkybile@gmail.com>
2023-01-27 11:31:42 -0800

    edited and added file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+/*
+ * file: 2-print_alphabet
+ * Auth: Tanner Burkybile <tanner.burkybile@gmail.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char x
+	int y
+
+	for (y = 0 ; y <= 9 ; y++)
+	{
+
+		for (x = 'a' ; x <= 'z' ; x++)
+		{
+			_putchar(x)
+		}
+		putchar ('\n')
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef HEADER_H
 #define HEADER_H
+void print_alphabet_x10(void);
 int _putchar(char c);
 void print_alphabet(void);
 #endif
