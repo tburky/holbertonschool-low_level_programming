0952c205c232c9637d0b46b06903860e77b16727
Tanner <tanner.burkybile@gmail.com>
2023-01-27 12:45:08 -0800

    edited file

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,10 +3,10 @@
 #include "main.h"
 
 /**
- * _islower - Entry point
- * @c
- * Return: Always 0 (Success)
- */
+* _islower - Entry point
+* @c
+* Return: Always 0 (Success)
+*/
 int _islower(int c)
 {
 	if (c > 96 && c <= 122)
