8a196a42e43830f4ed2529c929fa1e589aeffa89
Tanner <tanner.burkybile@gmail.com>
2023-01-27 09:43:38 -0800

    added new file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+/*
+ * file: 0-putchar.c
+ * Auth: Tanner Burkybile <tanner.burkybile@gmail.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is a directory regarding loops and nested loops in C
