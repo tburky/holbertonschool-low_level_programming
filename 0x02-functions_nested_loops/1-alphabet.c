4a7fffa0e6576c947ec6a4e120ade4f00546aac5
Tanner <tanner.burkybile@gmail.com>
2023-01-27 10:29:02 -0800

    edited and added file 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+/*
+ * file: 1-alphabet.c
+ * Auth: Tanner Burkybile <tanner.burkybile@gmail.com>
+ */
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	_putchar(ch);
+
+	_putchar(10);
+}
