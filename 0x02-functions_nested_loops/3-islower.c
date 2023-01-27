bcabf18151972ba32ce9c4d64079ab94a3c6080f
Tanner <tanner.burkybile@gmail.com>
2023-01-27 12:37:21 -0800

    edited file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,25 @@
+/*
+ * file: 3-islower.c
+ * Auth: Tanner Burkybile <tanner.burkybile@gmail.com>
+ */
+
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int _islower(int c);
+{
+	
+	if (c > 96 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,8 @@
 #ifndef HEADER_H
 #define HEADER_H
+int _islower(int c);
 void print_alphabet_x10(void);
 int _putchar(char c);
 void print_alphabet(void);
+int _islower(int c);
 #endif
