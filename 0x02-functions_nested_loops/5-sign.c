3e6e5a7631fc563617f162b6739545cda73886fb
Tanner <tanner.burkybile@gmail.com>
2023-01-30 10:49:26 -0800

    edited file

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,18 +9,18 @@
 int print_sign(int n)
 {
 	if (n > 0)
-		_putchar (43);
 	{
+		_putchar (43);
 		return (1);
 	}
 	if (n == 0)
-		_putchar (48);
 	{
+		_putchar (48);
 		return (0);
 	}
 	if (n < 0)
-		_putchar (45);
 	{
+		_putchar (45);
 		return (-1);
 	}
 }
