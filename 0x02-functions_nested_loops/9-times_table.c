176b147ab6c5ac3649749b35a70480e73e04ddcc
Tanner <tanner.burkybile@gmail.com>
2023-01-30 13:26:12 -0800

    edited file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,9 +19,9 @@ void times_table(void)
 {
 	int x, y, z;
 
-	for (x = 0 ; x < 10 ; x++)
+	for (x = 0 ; x < 10 ; ++x)
 		{
-		for (y = 0; y < 10 ; x++)
+		for (y = 0; y < 10 ; ++y)
 		{
 			z = x * y;
 
