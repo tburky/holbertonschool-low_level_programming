ae87d7df3b4916beae8ca040ab5b3500e4557a40
Tanner <tanner.burkybile@gmail.com>
2023-01-30 11:38:58 -0800

    added #6 for testing

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_sign - Entry point
+* Return: Absolute value
+*/
+int _abs(int n)
+{
+abs(n) = ans;
+	_putchar (ans);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef HEADER_H
 #define HEADER_H
+int _abs(int);
 int print_sign(int n);
 int _isalpha(int c);
 int _islower(int c);
