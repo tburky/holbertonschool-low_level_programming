54c181f99695b439b6213d48e784eb35caede213
Tanner <tanner.burkybile@gmail.com>
2023-01-31 08:43:18 -0800

    edited file and added

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,23 @@
+/*
+* File 10-add.c
+* Auth: Tanner Burkybile
+*/
+
+#include <stdio.h>
+#include "main.h"
+#include <math.h>
+
+/**
+* add - that is really weird
+* @n1: first one
+* @n2: second one
+* Description: adding
+* Return: returned
+*/
+int add(int n1, int n2)
+{
+	int sum;
+
+	sum = n1 + n2;
+	return (sum);
+}
