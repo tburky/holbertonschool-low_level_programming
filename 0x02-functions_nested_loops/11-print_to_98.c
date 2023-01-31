a1461a063f06f084c7a0577a0d92283eab3c978d
Tanner <tanner.burkybile@gmail.com>
2023-01-31 08:20:00 -0800

    edited file

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+/*
+* File 11-print_to_98.c
+* Auth: Tanner Burkybile
+*/
+
+#include <stdio.h>
+#include "main.h"
+#include <math.h>
+
+/**
+* print_to_98 - print 98
+* @n: n is the var
+* Description: 98
+* Void:
+*/
+
+void print_to_98(int n)
+{
+if (n >= 98)
+	{
+	while (n > 98)
+		printf("%d, ", n--);
+	printf("%d\n", n);
+	}
+}
+else
+{
+	while (n < 98)
+		printf("%d", ', n++);
+	printf("%d\n", n);
+}
+}
